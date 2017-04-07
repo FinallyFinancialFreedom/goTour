5b44675e31c937a8c0ba81270d921cf47251f029
liyang27 <liyang27@le.com>
2017-04-07 15:53:39 +0800

    basic channel go

--- hello.go
+++ hello.go
@@ -2,37 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	task "tourBase/subTask"
 )
 
 func main() {
-	//e := task.MyFloat64(-2.718)
-	caoliu := task.MyInt(1024)
-	//fmt.Println(e.Abs())
-	fmt.Println(caoliu.AbsInt())
-
-	var a task.Abser
-	f := task.MyFloat64(2.718)
-	a = &f
-	fmt.Printf("%T", a)
-	fmt.Println()
-	fmt.Println(a.AbsInt())
-
-	//notice what happen if there is no * sign below.
-	underConcreteValue := a.(*task.MyFloat64)
-	fmt.Printf("%T", underConcreteValue)
-
-	fmt.Println()
-	str1 := 127
-	str2 := 0
-	fmt.Print(strconv.Itoa(str1) + "." + strconv.Itoa(str2))
-
-	i, err := parseInt("x")
-	fmt.Print(err)
-	fmt.Print(i)
+	c := make(chan int)
+	//by default,send and receiver blocks when the other side is not ready
+	//so you can not write to and read from a same channel in same goroutine step by step, it causes deadlock.
+	go write(c)
+	fmt.Print(read(c))
 }
 
-func parseInt(v string) (int, error) {
-	return strconv.Atoi(v)
+func write(c chan int) {
+	fmt.Println("write to channel")
+	c <- 1
+}
+
+func read(c chan int) int {
+	fmt.Println("read from channel")
+	return <-c
 }
