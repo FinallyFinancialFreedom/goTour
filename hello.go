6e6f9b7092c1e60fb350d3e4af09fa7663985a80
liyang27 <liyang27@le.com>
2017-04-07 16:23:22 +0800

    basic thread no channel

--- hello.go
+++ hello.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"time"
 )
 
 func main() {
-	c := make(chan int)
 	//by default,send and receiver blocks when the other side is not ready
 	//so you can not write to and read from a same channel in same goroutine step by step, it causes deadlock.
+	//One can not guarantee a go func to be execute, maybe the main thread is over before CPU has time to call go func
+	c := 12
 	go write(c)
-	fmt.Print(read(c))
+	go read(c)
+	time.Sleep(10000 * time.Nanosecond)
 }
 
-func write(c chan int) {
-	fmt.Println("write to channel")
-	c <- 1
+func write(c int) {
+	fmt.Println("write to channel" + strconv.Itoa(c))
 }
 
-func read(c chan int) int {
-	fmt.Println("read from channel")
-	return <-c
+func read(c int) int {
+	fmt.Println("read from channel" + strconv.Itoa(c))
+	return c
 }
