956e38eb60fee8df0dfe4cce73d97b02e611c820
liyang27 <liyang27@le.com>
2017-04-07 18:11:12 +0800

    demostrate read will wait the write operation

--- hello.go
+++ hello.go
@@ -9,8 +9,9 @@ func main() {
 	c := make(chan int)
 	//by default,send and receiver blocks when the other side is not ready
 	//so you can not write to and read from a same channel in same goroutine step by step, it causes deadlock.
+	//and code below demostrate that read will not return until write func write a value to channel.
 	go read(c)
-	time.Sleep(5 * time.Second)
+	time.Sleep(2 * time.Second)
 	go write(c)
 	time.Sleep(10 * time.Millisecond)
 }
